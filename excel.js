97c56d57fe78097622be438ab21a247f8f0b54bf
Priyankasac <154412837+Priyankasac@users.noreply.github.com>
2024-02-13 10:32:33 +0530

    Update excel.js

--- excel.js
+++ excel.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "https://priyankayala.github.io/ExcelUpload/xlsx.js";
+            let xlsxjs = "https://Priyankasac.github.io/Excel-Upload/xlsx.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
